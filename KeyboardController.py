02ff93779a9cbb7a59c50f18003ef6510f01c127
BradJKim <bradjkim03@gmail.com>
2026-03-25 21:02:15 -0700

    Keyboard input change

--- KeyboardController.py
+++ KeyboardController.py
@@ -1,12 +1,24 @@
-import readchar
+import sys
+import tty
+import termios
 
 class KeyboardController:
     def __init__(self):
         pass
-    
-    def grabInputChar (self):
+
+    def grabInputChar(self):
         try:
-            return readchar.readkey()
+            fd = sys.stdin.fileno()
+            old_settings = termios.tcgetattr(fd)
+
+            try:
+                tty.setraw(fd)
+                ch = sys.stdin.read(1)
+            finally:
+                termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+
+            return ch
+
         except Exception as e:
-            print("Keyboard Controller Error: ", e)
+            print("Keyboard Controller Error:", e)
             return False
